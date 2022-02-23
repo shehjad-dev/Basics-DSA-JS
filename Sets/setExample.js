5fb51c0c12dad116356342798f783ff12166d86d
shehjad-dev <satauskz@gmail.com>
2022-02-23 18:16:39 +0600

    setExample added

---
+++ Sets/setExample.js
@@ -0,0 +1,37 @@
+//colleced from codewars
+/* An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true
+"aba" --> false
+"moOse" --> false (ignore letter case) */
+
+// So, now we can do this in the following way with a brute force solution =>
+/* 
+function isIsogram(str) {
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+} 
+*/
+
+// But rather we can use set to simplify the function as below
+function isIsogram(str) {
+  return new Set(str.toLowerCase()).size == str.length;
+}
+
+const demoStr = 'awesom';
+console.log(
+  `${
+    isIsogram(`${demoStr}`)
+      ? `${demoStr} is an isogram`
+      : `Oh no! ${demoStr} is not an isogram`
+  }`
+);
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 // run `node index.js` in the terminal
 console.log(`Lets learn the basics of DSA with Javascript`);
+
+
