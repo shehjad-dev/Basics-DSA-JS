55106137cc2f58f7e167bd435576e51c297663ef
shehjad-dev <satauskz@gmail.com>
2022-03-08 03:03:21 +0600

    updated question for priority queue example

--- PriorityQueue/priorityQueueExample.js
+++ PriorityQueue/priorityQueueExample.js
@@ -1,4 +1,7 @@
-// use priority queue and create a custom function to
-// set up priorities of child, female & male as 1, 2 & 3
+// use priority queue and update its enqueue function by
+// setting up priorities of child, female & male as 1, 2 & 3
 // during a moment of emergency shifting from one country
 // to another by boat.
+
+
+
